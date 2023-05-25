9adcd30299cbd871937d49f846ac737545b0bde2
Minnael <minnaelcampelo@gmail.com>
2023-05-25 15:52:09 -0300

    testes com a classe sculptor.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,20 @@
 
 int main(){
     Sculptor cubo(100, 100, 100);
-    cubo.setColor(1, 0, 0, 1);
-    cubo.putEllipsoid(50, 50, 50, 50, 50, 40);
-    cubo.cutEllipsoid(50, 50, 50, 49, 51, 39);
 
+    cubo.setColor(1, 0, 0, 1);
+    cubo.putBox(40, 60, 40, 60, 30, 70);
+    cubo.cutBox(41, 59, 41, 60, 30, 70);
+    cubo.setColor(0, 0, 1, 1);
+    cubo.putSphere(50, 50, 50, 8);
+    cubo.cutSphere(50, 50, 50, 7);
+    /*cubo.setColor(1, 0, 0, 1);
+    cubo.putEllipsoid(30, 30, 30, 30, 45, 45);
+    cubo.putEllipsoid(30, 30, 30, 29, 44, 44);
+    cubo.setColor(0, 0, 1, 1);
+    cubo.putSphere(50, 50, 50, 30);
+    cubo.cutSphere(50, 50, 50, 29);
+    */
     cubo.writeOFF("ellipsoid.off");
 
     return 0;
