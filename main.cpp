2b6bc88e34c055661909febe9f8dde280494c2e9
Minnael <minnaelcampelo@gmail.com>
2023-05-28 16:40:47 -0300

    finalização do teste em conjunto com o doxygen

--- main.cpp
+++ main.cpp
@@ -5,53 +5,59 @@
 int main(){
     Sculptor teste(100, 100, 100);
 
+    //DEFININDO UM CUBO SIMPLES
+    teste.setColor(0, 1, 0, 1);
+    teste.putBox(35, 65, 60, 90, 35, 65);
+    //DEFININDO UM CORTE DENTRO DO CUBO
+    teste.cutBox(36, 64, 61, 89, 36, 64);
+
+
     //DEFININDO VOXEL´S SIMPLES
     teste.setColor(1, 0, 0, 1);
-    teste.putVoxel(50, 50, 50);
+    teste.putVoxel(50, 75, 50);
 
     teste.setColor(0, 1, 0, 1);
-    teste.putVoxel(51, 50, 50);
+    teste.putVoxel(51, 75, 50);
 
     teste.setColor(0, 0, 1, 1);
-    teste.putVoxel(49, 50, 50);
+    teste.putVoxel(49, 75, 50);
 
     teste.setColor(1, 1, 0, 1);
-    teste.putVoxel(52, 50, 50);
+    teste.putVoxel(52, 75, 50);
 
     teste.setColor(0, 1, 1, 1);
-    teste.putVoxel(48, 50, 50);
+    teste.putVoxel(48, 75, 50);
 
     teste.setColor(1, 0, 1, 1);
-    teste.putVoxel(53, 50, 50);
+    teste.putVoxel(53, 75, 50);
 
     teste.setColor(1, 1, 1, 1);
-    teste.putVoxel(47, 50, 50);
+    teste.putVoxel(47, 75, 50);
 
     teste.setColor(0, 0, 0, 1);
-    teste.putVoxel(50, 51, 50);
+    teste.putVoxel(50, 76, 50);
+
+    //UTILIZANDO A EXCLUSÃO DE UM VOXEL
+    teste.setColor(1, 0, 0, 1);
+    teste.putVoxel(46, 50, 50);
+    teste.cutVoxel(46, 50, 50);
+
 
     //DEFININDO UMA ESFERA BÁSICA
+    teste.setColor(0, 0, 1, 1);
+    teste.putSphere(85, 75, 50, 15);
+    //DEFININDO UM CORTE DENTRO DA ESFERA
+    teste.cutSphere(85, 75, 50, 14);
+
+
+    //DEFININDO UM ELIPSOIDE SIMPLES
     teste.setColor(1, 0, 0, 1);
-    teste.putSphere(50, 85, 50, 29);
-
-    //DEFININDO UM CORTE NA ESFERA CRIADA
-    teste.cutSphere(50, 75, 50, 19);
-
+    teste.putEllipsoid(14, 75, 50, 15, 15, 25);
+    //DEFININDO UM CORTE DENTRO DE UM ELIPSOIDE
+    teste.cutEllipsoid(14, 75, 50, 14, 14, 24);
 
 
-    /*teste.putVoxel();
-    teste.cutVoxel();
-
-    teste.putBox();
-    teste.cutBox();
-
-    teste.putSphere();
-    teste.cutSphere();
-
-    teste.putEllipsoid();
-    teste.putEllipsoid();*/
-
-    teste.writeOFF("ellipsoid.off");
+    teste.writeOFF("teste.off");
 
     return 0;
 }
